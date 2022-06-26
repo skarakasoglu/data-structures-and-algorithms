60b0d1ede9178b2ece2fede4d068bfcc898a5577
Süleyman Karakaşoğlu <suleyman.karakasoglu@getir.com>
2022-06-26 20:28:10 +0300

    implemented the solution for 78. Subsets problem

---
+++ neetcode/backtracking/subsets.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Süleyman Karakaşoğlu on 26.06.2022.
+//
+
+#include <vector>
+
+void dfs(std::vector<std::vector<int>>& combinations, std::vector<int> current_combination, std::vector<bool>& visited, int index, std::vector<int>& nums) {
+    if (index >= nums.size()) return;
+
+    for (auto i = index; i  < nums.size(); i++) {
+        current_combination.emplace_back(nums[i]);
+        dfs(combinations, current_combination, visited, index + 1, nums);
+        combinations.emplace_back(current_combination);
+        current_combination.pop_back();
+    }
+}
+
+std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
+    std::vector<std::vector<int>> res;
+    res.emplace_back(std::vector<int>{});
+    std::vector<bool> visited(nums.size(), false);
+    dfs(res, {}, visited, 0, nums);
+    return res;
+}
