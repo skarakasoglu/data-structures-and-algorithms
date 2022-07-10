633dc69e69e2ff4c374766cc913b794c6e6aa7aa
Süleyman Karakaşoğlu <xtinasfighters@gmail.com>
2022-07-10 15:54:20 +0300

    Implemented the solution for Kth Smallest Element in a BST problem in LeetCode

--- neetcode/tree/k_th_smallest_element_in_a_bst.cpp
+++ neetcode/tree/k_th_smallest_element_in_a_bst.cpp
@@ -1,3 +1,37 @@
 //
 // Created by Süleyman Karakaşoğlu on 6.07.2022.
 //
+#include <vector>
+#include <stack>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int kthSmallest(TreeNode* root, int k) {
+    std::stack<TreeNode*> nodes;
+    auto n = 0;
+    auto current = root;
+
+    while (current || !nodes.empty()) {
+        while (current) {
+            nodes.push(current);
+            current = current->left;
+        }
+
+        current = nodes.top();
+        n++;
+        if (n == k) {
+            return current->val;
+        }
+        current = current->right;
+        nodes.pop();
+    }
+
+    return -1;
+}
